752ea3290502c1eb37f51f8153b2de2bea4fd4bd
VasaviSure <vasavime0510@gmail.com>
2024-07-21 04:14:16 -0700

    admin order

--- admin/src/pages/Orders/Orders.jsx
+++ admin/src/pages/Orders/Orders.jsx
@@ -1,7 +1,27 @@
 import React from 'react'
 import './Orders.css'
+import { useState } from 'react'
+import toast from 'react-toastify'
+import { useEffect } from 'react'
+import axios from 'axios'
 
 const Orders = () => {
+  const [orders,setOrders]=useState([])
+
+  const fetchAllOrders=async()=>{
+    const response=await axios.get(url+'/api/order/list')
+    if(response.data.success){
+      setOrders(response.data.data)
+      console.log(response.data.data)
+    }
+    else{
+      toast.error("Error")
+    }
+  }
+
+  useEffect(()=>{
+ fetchAllOrders();
+  },[])
   return (
     <div>
       
